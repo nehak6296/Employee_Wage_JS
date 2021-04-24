6a00dfba12150aa597ea95e307fae1177077901b
Neha <nehak6296@gmail.com>
2021-04-24 11:30:40 +0530

    [Neha]Added:Calculated wage till working hours of 160 or max days of 20 is reached for month

--- EmpWageUsingSwitch.js
+++ EmpWageUsingSwitch.js
@@ -26,7 +26,7 @@ let empCheck=Math.floor(Math.random()*10)%3;
 empHrs=getWorkingHours(empCheck);
 let empWage=empHrs*WAGE_PER_HOUR;
 console.log("UC3 - Hour: "+empHrs+" Emp Wage: "+empWage);
-*/
+
 //UC4
 let totalEmpHrs=0;
  for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
@@ -36,4 +36,23 @@ let totalEmpHrs=0;
  }
 
  let empWage=totalEmpHrs*WAGE_PER_HOUR;
- console.log("Hour: "+totalEmpHrs+" Emp Wage: "+empWage);
+ console.log("UC4 - Hour: "+totalEmpHrs+" Emp Wage: "+empWage);
+*/
+//UC5
+{
+      const MAX_HRS_IN_MONTH=160;
+      const NUM_OF_WORKING_DAYS=20;
+      let totalEmpHrs=0;
+      
+      let totalWorkingDays=0;
+      while(totalEmpHrs <= MAX_HRS_IN_MONTH && 
+            totalWorkingDays < NUM_OF_WORKING_DAYS)
+      {
+         totalWorkingDays++;
+         let empCheck = Math.floor(Math.random()*10)%3;
+         totalEmpHrs+=getWorkingHours(empCheck);
+      }
+     let empWage = totalEmpHrs*WAGE_PER_HOUR;
+     console.log("UC5 - Total Days: "+totalWorkingDays+
+     " Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage); 
+}
